fba7bfa54f9bd8e1383ac226acc790463ee68364
Estelope <elopez14567@gmail.com>
2023-11-01 23:38:14 -0600

    add models +validation

---
+++ Bakery/Models/Flavor.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bakery.Models 
+{
+    public class Flavor
+    {
+        public int FlavorId { get; set; }
+        
+        [Required(ErrorMessage = "Flavor must have a name: please add a name.")]
+        public string Name { get; set; }
+
+        public List<FlavorTreat> FlavorTreats { get; set; }
+    }
+}
---
+++ Bakery/Models/Treat.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bakery.Models 
+{
+    public class Treat
+    {
+        public int TreatId { get; set; }
+        
+        [Required(ErrorMessage = "Treat must have a name: please add a name.")]
+        public string Name { get; set; }
+
+        public List<FlavorTreat> FlavorTreats { get; set; }
+    }
+}
